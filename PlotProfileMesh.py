fc7539567125e8f9a4e75559a23792fddf5ef31f
lichen <lichenmechanics@163.com>
2021-06-04 10:24:59 +0200

    correct name of generated png file in PlotProfileMesh.py

--- PlotProfileMesh.py
+++ PlotProfileMesh.py
@@ -19,44 +19,49 @@ def PlotSaveHollowTri(CroSecFolder):
         raise FileNotFoundError(shift2centroidFile + 'not exist!')
 
     extract = LoadNumericMatrixInMatFile(shift2centroidFile, ['Po', 'Pi'])
-    Po = extract['Po']
-    Pi = extract['Pi']
+    Po = extract['Po']          # vertices on external bound
+    Pi = extract['Pi']          # vertices on internal bound
 
 
     # load mesh
     coords_ien_bgpFile = os.path.join(CroSecFolder, 'tri_mesh', 'coords_ien_bgp.mat')
     extract = LoadNumericMatrixInMatFile(coords_ien_bgpFile, ['ien', 'coords'])
-    ien = extract['ien']
-    coords = extract['coords']
+    ien = extract['ien']        # index starts from 1
+    coords = extract['coords']  # x y coordinates of mesn nodes
 
-
+    # start plotting
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
     ax.set_aspect(aspect="equal")
 
-    # outer profile
-    x = Po[:, 0].tolist(); x.append(x[0])
-    y = Po[:, 1].tolist(); y.append(y[0])
-    ax.plot(x, y, 'go--', linewidth=1, markersize=12, label='outer profile')
+    # plot outer bound
+    x = Po[:, 0].tolist()
+    x.append(x[0])
+    y = Po[:, 1].tolist()
+    y.append(y[0])
+    ax.plot(x, y, 'go--', linewidth=1, label='outer profile')
     for i in range(3):
-        ax.text(x[i], y[i], str(i), fontsize=12)    # add node index
+        ax.text(x[i], y[i], str(i), fontsize=12)    # annotate node index
 
-    # inner profile
-    x = Pi[:, 0].tolist(); x.append(x[0])
-    y = Pi[:, 1].tolist(); y.append(y[0])
-    ax.plot(x, y, 'ro--', linewidth=1, markersize=12, label='inner profile')
+    # plot inner bound
+    x = Pi[:, 0].tolist()
+    x.append(x[0])
+    y = Pi[:, 1].tolist()
+    y.append(y[0])
+    ax.plot(x, y, 'ro--', linewidth=1, label='inner profile')
 
+    # plot triangular mesh
     ax.triplot(coords[:, 0], coords[:, 1], ien - 1, linewidth=0.1)
 
-    ax.plot(0, 0, 'rs', markersize=12)          # plot Origin of cross section
+    ax.plot(0, 0, 'rs', markersize=6)          # plot Origin of cross section
 
     now = datetime.now()
-    current_time = now.strftime("%H-%M-%S")
-    pngFile = CroSecFolder.split('/')[-1] + '_' + current_time + '.png'
-    pngFile = os.path.join(CroSecFolder, pngFile)
-    fig.savefig(pngFile,dpi=300, transparent=True)      # save the figure
+    current_time = now.strftime("%Y-%m-%d-%H-%M-%S")    # year-month-day-hour-min-sec
+    pngFile = CroSecFolder.split('/')[-1] + '_' + current_time + '.png' # name of png file
+    pngFile = os.path.join(CroSecFolder, pngFile)       # full path
+    fig.savefig(pngFile, dpi=300, transparent=True)
 
-    plt.close("all")
+    plt.close(fig)
     return
 
 
